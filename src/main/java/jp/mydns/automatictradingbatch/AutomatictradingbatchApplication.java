97c8deb3355a89f9b5346eadcd8e7bbd800c0bac
shigeta <geshi2soccer@gmail.com>
2022-08-22 23:13:07 +0900

    初回

---
+++ src/main/java/jp/mydns/automatictradingbatch/AutomatictradingbatchApplication.java
@@ -0,0 +1,13 @@
+package jp.mydns.automatictradingbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatictradingbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatictradingbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/mydns/automatictradingbatch/AutomatictradingbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.mydns.automatictradingbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatictradingbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
